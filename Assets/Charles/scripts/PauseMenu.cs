da2db1a8b5ed5609070117c14a7dcb3831ec4798
MUKILTEO\1725289 <charles82508@gmail.com>
2026-03-16 08:39:22 -0700

    pause menu works                    -

--- Assets/Charles/scripts/PauseMenu.cs
+++ Assets/Charles/scripts/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(true); 
         Time.timeScale = 0f; 
     }
-    public void Resume(InputAction.CallbackContext ctx)
+    public void Resume()
     {   
         isPaused = false;
         pauseMenu.SetActive(false);
@@ -29,6 +30,7 @@ public class PauseMenu : MonoBehaviour
     
     public void back()
     {
+        SceneManager.LoadScene("MainMenu");
         Time.timeScale = 1f; // Resume the game by setting time scale back to 1
     }   
 }
