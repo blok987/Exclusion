dc03ec4b81c027ecd6275771ceed2508c299cb32
MUKILTEO\1725203 <)>
2026-03-26 09:30:45 -0700

    Fixed random damge for limbs

--- Assets/Ben/RandomDamageGeneratorScripts/RandomLegDamage.cs
+++ Assets/Ben/RandomDamageGeneratorScripts/RandomLegDamage.cs
@@ -4,40 +4,63 @@ public class RandomLegDamage : MonoBehaviour
 {
     public HealthLeg1 playerLeg1Health;
     public HealthLeg2 playerLeg2Health;
+
+    public GameObject playerLeg1;
+    public GameObject playerLeg2;
+
+    private bool isLeg1Dead = false;
+    private bool isLeg2Dead = false;
+
     public PlayerBodyCollision playerBodyCollision;
     private int rd;
 
+    private void FixedUpdate()
+    {
+        if (playerLeg1 == null)
+        {
+            isLeg1Dead = true;
+        }
+
+        if (playerLeg2 == null)
+        {
+            isLeg2Dead = true;
+        }
+    }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
        
-        if (collision.gameObject.CompareTag("hurt"))
+        if (collision.gameObject.transform.CompareTag("hurt") )
         {
             rd = Random.Range(1, 50);
 
-            if (rd <= 25)
+            if (rd <= 25 && isLeg1Dead == false)
             {
                 playerLeg1Health.TakeDamage(2);
                 Debug.Log("Leg 1 took damage");
+                Debug.Log("rd is " + rd);
                 playerBodyCollision.isLeg1Colliding = true;
             }
-            else if (rd <= 25 && gameObject.transform.childCount < 2)
+            if (rd <= 25 && isLeg1Dead == true)
             {
                 playerLeg2Health.TakeDamage(2);
                 playerBodyCollision.isLeg2Colliding = true;
+                Debug.Log("rd is " + rd);
             }
 
-            if (rd >= 26)
+            if (rd >= 26 && isLeg2Dead == false)
             {
                 playerLeg2Health.TakeDamage(2);
                 Debug.Log("Leg 2 took damage");
                 playerBodyCollision.isLeg2Colliding = true;
+                Debug.Log("rd is " + rd);
             }
-            else if (rd >= 26 && gameObject.transform.childCount < 2)
+            if (rd >= 26 && isLeg2Dead == true)
             {
                 playerLeg1Health.TakeDamage(2);
                 playerBodyCollision.isLeg1Colliding = true;
+                Debug.Log("rd is " + rd);
             }
         }
     }
@@ -46,7 +69,7 @@ public class RandomLegDamage : MonoBehaviour
     {
         if (gameObject.transform.childCount <= 0)
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
     }
 }
