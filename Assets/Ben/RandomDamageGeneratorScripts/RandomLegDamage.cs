43b2a111188f10e0207cf0bd957050653cb9bcfb
MUKILTEO\1725203 <)>
2026-03-26 08:20:36 -0700

    finished random leg damage

--- Assets/Ben/RandomDamageGeneratorScripts/RandomLegDamage.cs
+++ Assets/Ben/RandomDamageGeneratorScripts/RandomLegDamage.cs
@@ -11,32 +11,35 @@ public class RandomLegDamage : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        rd = Random.Range(1, 100);
+       
+        if (collision.gameObject.CompareTag("hurt"))
+        {
+            rd = Random.Range(1, 50);
 
-        if (rd <= 50)
-        {
-            playerLeg1Health.TakeDamage(2);
-            Debug.Log("Arm 1 took damage");
-            playerBodyCollision.isLeg1Colliding = true;
-        }
-        else if (rd <= 50 && gameObject.transform.childCount < 2)
-        {
-            playerLeg2Health.TakeDamage(2);
-            playerBodyCollision.isLeg2Colliding = true;
-        }
+            if (rd <= 25)
+            {
+                playerLeg1Health.TakeDamage(2);
+                Debug.Log("Leg 1 took damage");
+                playerBodyCollision.isLeg1Colliding = true;
+            }
+            else if (rd <= 25 && gameObject.transform.childCount < 2)
+            {
+                playerLeg2Health.TakeDamage(2);
+                playerBodyCollision.isLeg2Colliding = true;
+            }
 
-        if (rd >= 51)
-        {
-            playerLeg2Health.TakeDamage(2);
-            Debug.Log("Arm 2 took damage");
-            playerBodyCollision.isLeg2Colliding = true;
+            if (rd >= 26)
+            {
+                playerLeg2Health.TakeDamage(2);
+                Debug.Log("Leg 2 took damage");
+                playerBodyCollision.isLeg2Colliding = true;
+            }
+            else if (rd >= 26 && gameObject.transform.childCount < 2)
+            {
+                playerLeg1Health.TakeDamage(2);
+                playerBodyCollision.isLeg1Colliding = true;
+            }
         }
-        else if (rd >= 51 && gameObject.transform.childCount < 2)
-        {
-            playerLeg1Health.TakeDamage(2);
-            playerBodyCollision.isLeg1Colliding = true;
-        }
-
     }
 
     void Update()
