d691ad172978b2067797fa3d18c34348a134feed
MUKILTEO\1726358 <bloked987@gmail.com>
2026-04-13 10:18:51 -0700

    Began the framework for the monetary system NOTES: make a working prototype of money

---
+++ Assets/George/Scripts/Monetary Sys.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MonetarySys : MonoBehaviour
+
+{
+    public int Spall;
+    [SerializeField] bool HasSpall;
+    public int SpallValue;
+    [SerializeField] int Spallet;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private int SpallValue()
+    {
+        
+    }
+}
