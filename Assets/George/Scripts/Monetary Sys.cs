d5f4ebd3641b7ea58e703e17d9f7fda9f1cf3c0f
MUKILTEO\1726358 <bloked987@gmail.com>
2026-04-14 10:08:42 -0700

    Worked on Money NOTES: figure out how to utilize collisions or raycasts to detect other objects

--- Assets/George/Scripts/Monetary Sys.cs
+++ Assets/George/Scripts/Monetary Sys.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 public class MonetarySys : MonoBehaviour
 
 {
-    public int Spall;
     [SerializeField] bool HasSpall;
     public int SpallValue;
     [SerializeField] int Spallet;
+    GameObject Spall;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        SpallValue = 1;
+
+    }
+    private void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
 
-    // Update is called once per frame
-    //void ()
-    //{
-        
-    //}
+
+
 }
