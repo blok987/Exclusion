9c0775fca7045eb8355bf1910d26d0d539775108
MUKILTEO\1725203 <)>
2026-04-15 10:19:16 -0700

    Degredation Sprites implemented

--- Assets/Ben/Scripts/HealthLimbsScripts/Health.cs
+++ Assets/Ben/Scripts/HealthLimbsScripts/Health.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,36 +9,81 @@ public class Health : MonoBehaviour
     public float maxHealth = 10;
     private float damageForDeath;
 
-  //  private WalkScript walkScript;
-
-   // public bool canTakeDamage = true;
+    public GameObject DollHead;
+    public GameObject DollChest;
+    public GameObject DollMidsection;
+    public GameObject DollWaist;
 
     public HealthBarBodyTest healthBar;
+
+    public Sprite DollHeadS;
+    public Sprite DollChestS;
+    public Sprite DollMidsectionS;
+    public Sprite DollWaistS;
+
+    public Sprite DollHeadBD;
+    public Sprite DollChestBD;
+    public Sprite DollMidsectionBD;
+    public Sprite DollWaistBD;
+
+    public Sprite DollHeadFD;
+    public Sprite DollChestFD;
+    public Sprite DollMidsectionFD;
+    public Sprite DollWaistFD;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        DollHeadS = Resources.Load<Sprite>("Limbs/NLimbs/Doll Head");
+        DollChestS = Resources.Load<Sprite>("Limbs/NLimbs/Doll Chest");
+        DollMidsectionS = Resources.Load<Sprite>("Limbs/NLimbs/Doll Midsection");
+        DollWaistS = Resources.Load<Sprite>("Limbs/NLimbs/Doll Waist");
+
+        DollHeadBD = Resources.Load<Sprite>("Limbs/BDLimbs/Doll Head DAMAGED");
+        DollChestBD = Resources.Load<Sprite>("Limbs/BDLimbs/Doll Chest DAMAGED");
+        DollMidsectionBD = Resources.Load<Sprite>("Limbs/BDLimbs/Doll Midsection DAMAGED");
+        DollWaistBD = Resources.Load<Sprite>("Limbs/BDLimbs/Doll Waist DAMAGED");
+
+        DollHeadFD = Resources.Load<Sprite>("Limbs/FDLimbs/Doll Head FULLY DAMAGED");
+        DollChestFD = Resources.Load<Sprite>("Limbs/FDLimbs/Doll Chest FULLY DAMAGED");
+        DollMidsectionFD = Resources.Load<Sprite>("Limbs/FDLimbs/Doll Midsection FULLY DAMAGED");
+        DollWaistFD = Resources.Load<Sprite>("Limbs/FDLimbs/Doll Waist FULLY DAMAGED");
+
         health = maxHealth;
-      //  canTakeDamage = true;
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (health > 5)
+        {
+            DollHead.GetComponent<SpriteRenderer>().sprite = DollHeadS;
+            DollChest.GetComponent<SpriteRenderer>().sprite = DollChestS;
+            DollMidsection.GetComponent<SpriteRenderer>().sprite = DollMidsectionS;
+            DollWaist.GetComponent<SpriteRenderer>().sprite = DollWaistS;
+        }
+        if (health <= 5 && health > 2)
+        {
+            DollHead.GetComponent<SpriteRenderer>().sprite = DollHeadBD;
+            DollChest.GetComponent<SpriteRenderer>().sprite = DollChestBD;
+            DollMidsection.GetComponent<SpriteRenderer>().sprite = DollMidsectionBD;
+            DollWaist.GetComponent<SpriteRenderer>().sprite = DollWaistBD;
+        }
+        if (health <= 2)
+        {
+            DollHead.GetComponent<SpriteRenderer>().sprite = DollHeadFD;
+            DollChest.GetComponent<SpriteRenderer>().sprite = DollChestFD;
+            DollMidsection.GetComponent<SpriteRenderer>().sprite = DollMidsectionFD;
+            DollWaist.GetComponent<SpriteRenderer>().sprite = DollWaistFD;
+        }
+
         if (gameObject.transform.childCount <= 0)
         {
             damageForDeath = health;
             health -= damageForDeath;
-            healthBar.UpdateHealth(damageForDeath);  
+            healthBar.UpdateHealth(damageForDeath);
         }
-
-        //if (canTakeDamage == true && walkScript.canMove)
-        //{
-        //    if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
-        //    {
-        //        StartCoroutine(WaitForDamage());
-        //    }
-        //}
 
         if (health <= 0)
         {
