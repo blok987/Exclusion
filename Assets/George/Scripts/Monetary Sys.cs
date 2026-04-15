5493933a19beafbe36cdf0f9e62aff56ecb95215
MUKILTEO\1726358 <bloked987@gmail.com>
2026-04-15 10:14:25 -0700

    Merge fix (Sybau)

--- Assets/George/Scripts/Monetary Sys.cs
+++ Assets/George/Scripts/Monetary Sys.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MonetarySys : MonoBehaviour
 
 {
     [SerializeField] bool HasSpall;
+    
     public int SpallValue;
     [SerializeField] int Spallet;
-    GameObject Spall;
     
+    public GameObject Spall;
+    
+    public LayerMask Player;
+    public LayerMask Collectible;
+    
+    public Vector3 CastOrigin;
+
+    public Vector2 Size;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,13 +25,24 @@ public class MonetarySys : MonoBehaviour
     }
     private void Update()
     {
-        
+        if (playerContact())
+        {
+            Debug.Log("Player Contact");
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         
     }
-
-
+    
+    [HideInInspector] bool playerContact()
+    {
+        return Physics2D.BoxCast(transform.position + CastOrigin , Size, 0, Vector2.zero, Player);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + CastOrigin, Size);
+    }
 
 }
