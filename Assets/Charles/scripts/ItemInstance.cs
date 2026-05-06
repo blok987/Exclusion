74fc63e21797f1dc14147a22e546d9af848e4ef5
child\Benjamin <1725203@mukilteo.wednet.edu>
2026-05-05 22:19:04 -0700

    no message

--- Assets/Charles/scripts/ItemInstance.cs
+++ Assets/Charles/scripts/ItemInstance.cs
@@ -39,7 +39,7 @@ public class ItemInstance : MonoBehaviour
         }
        
     }
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         
         if (other.gameObject.CompareTag("Player") & i.inventoryfull == false)
